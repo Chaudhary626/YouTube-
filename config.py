7cfe2f3e6a2208a4b09fe6cac0a37dfcac153b3c
MANISH CHAUDHARY <132375487+Chaudhary626@users.noreply.github.com>
2025-06-14 10:17:16 +0530

    Update and rename G to config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+BOT_TOKEN = "7342216134:AAEl99PlNXo56Vl0DDuSXC04Wq49IGsb2cw"
+DB_PATH = "yt_growth_bot.db"
+ADMIN_IDS = [5718213826]  # Replace with your Telegram user ID(s)
+MAX_VIDEOS = 5
+PROOF_TIMEOUT_SEC = 900  # 15 minutes
